8c8bb16d212f1a80b74a37d8411a853911057979
unknown <aeitor.cg@gmail.com>
2025-02-24 22:20:02 +0100

    dashboart + model principal

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $vista=$viewsController->obtenerVistasControllador($url[0]);
 if($vista =="login"||$vista=="404"){
     require_once "./app/views/content/".$vista."-view.php";
 }else{
+    require_once "./app/views/inc/navbar.php";
     require_once $vista;
 
 }
@@ -33,4 +34,5 @@ require_once "./app/views/inc/script.php";
 
 
 </body>
+
 </html>
