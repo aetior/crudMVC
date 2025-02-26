ed42ec46dac8244b3c3d494eb865f11ecbe84261
unknown <aeitor.cg@gmail.com>
2025-02-26 16:13:26 +0100

    preparado para crear controller crear usuario

---
+++ app/controllers/userController.php
@@ -0,0 +1,10 @@
+<?php
+    namespace app\controller;
+    use app\models\mainModel;
+
+    class userController extends mainModel{
+        # Controlador registar usario # 
+        public function registrarUsuarioControlador(){
+                
+        }
+    }
