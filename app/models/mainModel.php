17d7ef710da76340f7a12d7eeaffe490519d5f7b
unknown <aeitor.cg@gmail.com>
2025-02-26 02:37:57 +0100

    implementacion ajax

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -48,4 +48,130 @@
                     return true;
             }
         }
+
+        protected function guardarDatos($tabla,$datos){
+            $query="INSERT INTO $tabla (";
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){
+                    $query.=",";
+                }
+                $query.$clave["campo_nombre"];
+                $C++;
+            }
+            $query.=") VALUES(";
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){
+                    $query.=",";
+                }
+                $query.$clave["campo_marcador"];
+                $C++;
+            }
+            $query.=")";
+            $sql=$this->conectar()->prepare($query);
+
+            foreach($datos as $clave){
+                $sql->bindParam($clave["campo_marcador"],$clave["campo_valor"]);
+            }
+
+            $sql->execute();
+
+            return $sql;
+
+        }
+       
+        public function seleccionarDatos($tipo,$tabla,$campo,$id){
+            $tipo=$this->limpiarCadena($tipo);
+            $tabla=$this->limpiarCadena($tabla);
+            $campo=$this->limpiarCadena($campo);
+            $id=$this->limpiarCadena($id);
+
+            if($tipo=="Unico"){
+                $sql=$this->conetar()->prepare("SELECT * FROM $tabla where $campo=:ID");
+                $sql->bindParam(":ID",$id);
+            }elseif($tipo=="Normal"){
+                $sql=$this->conetar()->prepare("SELECT $campo FROM $tabla");
+            }
+
+            $sql->execute();
+            return $sql;
+
+        }
+
+        protected function actualizarDatos($tabla,$datos,$condicion){
+            $query="UPDATE $tabla SET ";
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){
+                    $query.=",";
+                }
+                $query.$clave["campo_nombre"]."=".$clave["campo_marcador"];
+                $C++;
+            }
+            $query.="WHERE ".$condicion["condicion_campo"]."=".$condicion["condicion_marcador"];
+
+            $sql=$this->conectar->prepare($query);
+           
+            foreach($datos as $clave){
+                $sql->bindParam($clave["campo_marcador"],$clave["campo_valor"]);
+            }
+            $sql->bindParam($condicion["condicion_marcador"],$condicion["condicion_valor"]);
+
+            $sql->execute();
+            return $sql;
+        }
+
+        protected function eliminarRegistro($tabla,$campo,$id){
+            $sql=$this->conectar()->prepare("DELETE FROM $tabla WHERE $campo =:id");
+            $sql->bindParam(":id",$id);
+            $sql->execute();
+            return $sql;
+        }
+
+        protected function paginadorTablas($pagina,$numeroPagina,$url,$botones){
+            $tablas='<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+            if($paginas<=1){
+                $tablas.='
+                <a class="pagination-previous is-disabled" disabled >Anterior</a>
+	            <ul class="pagination-list">
+                ';
+            }else{
+                $tablas.='
+                <a class="pagination-previous" href="'.$url.($pagina-1).'/">Anterior</a>
+                <ul class="pagination-list">
+                    <li><a class="pagination-link" href="'.$url.'1/">1</a></li>
+                    <li><span class="pagination-ellipsis">&hellip;</span></li>
+                ';
+            }
+            $ci=0;
+            for($i=$pagina; $i<=$numeroPagina; $i++){
+                if($ci>=$botones){
+                   break; 
+                }
+                if($pagina==$i){
+                    $tabla.='<li><a class="pagination-link is-current" href="'.$url.$i.'">'.$i.'</a></li>';
+                }else{
+
+                }
+                $ci++;
+            }
+                if($pagina==$numeroPagina){
+                    $tabla.='
+                    </ul>
+                    	<a class="pagination-next is-disabled" disabled >Siguiente</a>
+                    ';
+                }else{
+                    $tabla.='
+                        <li><span class="pagination-ellipsis">&hellip;</span></li>
+                        <li><a class="pagination-link" href="'.$url.$numeroPagina.'/">'.$numeroPagina.'</a></li>
+                    </ul>
+                    	<a class="pagination-next" href="'.$url.($pagina+1).'/">Siguiente</a>
+
+                    ';
+                }
+                $tablas.='</nav>';
+                return $tabla;
+
+        }
     }
